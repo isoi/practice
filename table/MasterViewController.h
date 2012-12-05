4bc7e7253248efe83b5c4073312a3a8ea94b061f
礒井 友香 <isoi@lassicmac5-no-MacBook-Pro.local>
2012-12-05 14:26:53 +0900

    Initial Commit

---
+++ table/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  table
+//
+//  Created by 礒井 友香 on 2012/12/05.
+//  Copyright (c) 2012年 礒井 友香. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ table/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  table
+//
+//  Created by 礒井 友香 on 2012/12/05.
+//  Copyright (c) 2012年 礒井 友香. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
